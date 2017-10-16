45ce89a1d7a3548413e2b688f392f20d52e81d05
dcsoundscapes <dcsoundscapes@live.com>
2017-10-16 18:05:38 -0400

    routes 5.1

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
 
 	get 'static_pages/contact'
 
+	get 'static_pages/landing_page'
+
   get 'static_pages/index'
-  root 'static_pages#landing_page'
+  root 'static_pages#index'
 
 
 

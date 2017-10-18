2c974d9de9b7784d6326045e47178c8ad0ca981e
dcsoundscapes <dcsoundscapes@live.com>
2017-10-17 21:34:04 -0400

    forms 5.2 new

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,6 @@ class Product < ApplicationRecord
 end
 
 def self.search(search_term)
-    if Rails.env.development?
-      Product.where("name LIKE ?", "%#{search_term}%")
-    else
-       Product.where("name ilike ?", "%#{search_term}%")
-     end
-   end
+    like_operator = Rails.env.production? ? 'ilike' : 'like'
+    Product.where("name #{like_operator} ?", "%#{search_term}%")
+  end

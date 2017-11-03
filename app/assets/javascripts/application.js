d30435548d0dfafeb2dd578662a8fa2d0981951a
dcsoundscapes <dcsoundscapes@live.com>
2017-11-03 18:38:59 -0400

    Comments Controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require turbolinks
 //= require jquery3
 //= require_tree .
+//= require jquery.raty.js
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+	resources :products do
+  resources :comments
+end
   
   devise_for :users
   resources :users

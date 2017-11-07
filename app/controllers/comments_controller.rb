f37dd6b0b3b7892f62ef8a3e754caaf69aef8653
dcsoundscapes <dcsoundscapes@live.com>
2017-11-06 23:23:28 -0500

    comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,4 +9,10 @@ class CommentsController < ApplicationController
 
   def destroy
   end
+
+  private
+ 
+ 	def comment_params
+ 		params.require(:comment).permit(:user_id, :body, :rating)
+ 	end
 end

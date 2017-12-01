cea46feef3040120402c49c65520dd5b5f277958
dcsoundscapes <dcsoundscapes@live.com>
2017-12-01 12:10:48 -0500

    6.3

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+	describe UsersController, type: :controller do
+		before do
+		let(:user) { User.create!(email: "d.colozzi@gmail.com", password: "sslj9k") }
+		let(:user2) { User.create!(email: "cdefgabflat@yahoo.com", password: "sslj9k00") }
+
+	end
+
+
+	describe 'GET #show' do
+		context 'when a user is logged in' do
+			 before do
+        sign_in user
+       end
+       it 'loads correct user details' do
+        get :show, params: { id: user.id }
+        expect(assigns(:user)).to eq user
+        expect(response).to have_http_status(200)
+       end
+     	 it 'cant access other users show page' do
+        get :show, params: { id: user2.id }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+       end
+
+		end
+
+		context 'when a user is not logged in' do
+			it 'redirects to login' do
+        get :show, params: { id: user.id }
+        expect(response).to redirect_to(new_user_session_path
+    	end
+		end
+	end
+end
+
+

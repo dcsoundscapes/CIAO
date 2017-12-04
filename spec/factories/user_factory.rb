0866b9895480570f81c86784c36e11fb2ab9bbec
dcsoundscapes <dcsoundscapes@live.com>
2017-12-03 22:23:51 -0500

    6.5 Stripe

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,19 +1,20 @@
 FactoryBot.define do
 	sequence(:email) { |n| "user#{n}@example.com" }
   
-  factory :user do
-    email 
-    password "sslj9k00"
-    first_name "D"
-    last_name "C"
-    admin false
+  	factory :user do
+   	 email 
+   	 password "sslj9k00"
+   	 first_name "D"
+   	 last_name "C"
+    	admin false
 
-    factory :admin, class: User do
+    	factory :admin, class: User do
 
-    email 
-    password "sslj9k"
-    first_name "Dave"
-    last_name "Colozzi"
-    admin true
-  end
+    	email 
+    	password "sslj9k"
+    	first_name "Dave"
+    	last_name "Colozzi"
+    	admin true
+  	end
+	end
 end

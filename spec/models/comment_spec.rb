a1c3b5a938417717bae36418b5ea090d3e0693ca
dcsoundscapes <dcsoundscapes@live.com>
2017-12-18 17:52:30 -0500

    6.15

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe Comment do
+
+  context "not given full information" do
+
+    it "is not valid without a rating" do
+      @comment = FactoryGirl.build(:comment, rating: nil)
+      expect(@comment).not_to be_valid
+    end
+
+    # it "is not valid without a rating" do
+    #   expect(Comment.new(rating: nil)).not_to be_valid
+    # end
+
+    it "is not valid without text" do
+      expect(Comment.new(body: "")).not_to be_valid
+    end
+
+    it "is not valid without a user id" do
+      expect(Comment.new(user_id: nil)).not_to be_valid
+    end
+
+  end
+end
